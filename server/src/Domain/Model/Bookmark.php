fdbe7976b8c3c7a020e54b4dfcf7eafc07520168
Maxime COLIN <maxime.colin@elao.com>
2017-06-13 15:11:09 +0200

    Add bookmark model

---
+++ server/src/Domain/Model/Bookmark.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Domain\Model;
+
+class Bookmark
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $url;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * Bookmark constructor.
+     *
+     * @param string $url
+     * @param string $title
+     */
+    public function __construct(string $url, string $title)
+    {
+        $this->url   = $url;
+        $this->title = $title;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
