f080054e1f0b7f645f4cc6e46767944dd3e1e51b
Maxime COLIN <maxime.colin@elao.com>
2017-06-13 18:38:59 +0200

    Merge bookmark update and create in one mutation

--- server/src/Infrastructure/GraphQL/Mutator/BookmarkMutator.php
+++ server/src/Infrastructure/GraphQL/Mutator/BookmarkMutator.php
@@ -35,43 +35,24 @@ class BookmarkMutator
     }
 
     /**
-     * @param string $url
-     * @param string $title
+     * @param string   $url
+     * @param string   $title
+     * @param int|null $id
      *
      * @return array
      * @throws UserError
      */
-    public function createBookmark(string $url, string $title)
+    public function mutateBookmark(string $url, string $title, int $id = null) : array
     {
         try {
-            $command = new CreateBookmarkCommand($url, $title);
+            $command = $id === null
+                ? new CreateBookmarkCommand($url, $title)
+                : new UpdateBookmarkCommand($id, $url, $title);
 
             /** @var Bookmark $bookmark */
             $bookmark = $this->bus->handle($command);
 
-            return ['bookmark' => $this->normalizer->normalize($bookmark)];
-        } catch (DomainException $exception) {
-            throw new UserError($exception->getMessage());
-        }
-    }
-
-    /**
-     * @param int    $id
-     * @param string $url
-     * @param string $title
-     *
-     * @return array
-     * @throws UserError
-     */
-    public function updateBookmark(int $id, string $url, string $title)
-    {
-        try {
-            $command = new UpdateBookmarkCommand($id, $url, $title);
-
-            /** @var Bookmark $bookmark */
-            $bookmark = $this->bus->handle($command);
-
-            return ['bookmark' => $this->normalizer->normalize($bookmark)];
+            return $this->normalizer->normalize($bookmark);
         } catch (DomainException $exception) {
             throw new UserError($exception->getMessage());
         }
