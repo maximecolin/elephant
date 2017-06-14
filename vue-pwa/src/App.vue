b1798537bc9712dd19181c9d417fc88bfa2a9358
Maxime COLIN <maxime.colin@elao.com>
2017-06-14 17:45:59 +0200

    Home link on nav brand

--- vue-pwa/src/App.vue
+++ vue-pwa/src/App.vue
@@ -5,7 +5,7 @@
             <button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="#">Elephant</a>
+            <router-link class="navbar-brand" :to="{ name: 'Home' }">Elephant</router-link>
         </nav>
 
         <div class="container-fluid">
