7cee8e63be00d709f033d667a7fdfa34d451acce
Maxime COLIN <maxime.colin@elao.com>
2017-06-14 19:32:47 +0200

    Elephant logo

--- vue-pwa/src/App.vue
+++ vue-pwa/src/App.vue
@@ -5,7 +5,10 @@
             <button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <router-link class="navbar-brand" :to="{ name: 'Home' }">Elephant</router-link>
+            <router-link class="navbar-brand" :to="{ name: 'Home' }">
+              <img src="./assets/elephant.svg" width="30" height="30" alt="">
+              Elephant
+            </router-link>
         </nav>
 
         <div class="container-fluid">
