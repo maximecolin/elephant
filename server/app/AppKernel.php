e845dd81e8217c4a389b41aceba5cf7629fb18bc
Maxime COLIN <maxime.colin@elao.com>
2017-07-27 16:37:13 +0200

    Remove SensioFrameworkExtraBundle

--- server/app/AppKernel.php
+++ server/app/AppKernel.php
@@ -14,7 +14,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Overblog\GraphQLBundle\OverblogGraphQLBundle(),
             new League\Tactician\Bundle\TacticianBundle(),
         ];
